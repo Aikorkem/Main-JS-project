5784b5a7fb5110244e7726d1327c7254501558ca
Sanam-Rajaboopathi <122203583+Sanam-Rajaboopathi@users.noreply.github.com>
2023-02-04 11:06:30 +0000

    game function changes

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let userCount;
-let computerCount;
-const computerSelection = computerPlay();
+let userCount = 0;
+let computerCount =0;
 const winner = game();
 function computerPlay(){
     let randomNumber = Math.floor(Math.random()*3);
-    // console.log(randomNumber);
     switch(randomNumber){
         case 0:
             return "Rock";
@@ -22,43 +20,46 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection){
 
-    playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
-    console.log(computerSelection);
+    // playerSelection = playerSelection.toLowerCase();
+    // console.log(playerSelection);
+    // console.log(computerSelection);
     // if(playerSelection.equals(computerPlay)){
 
     // }  // logic when both the values are same
-    let randomNumber = Math.floor(Math.random()*2);
-    switch(randomNumber){
-        case 0:
-            return "You Lose! Computer Beats User";
-            break;
-        case 1:
-            return "You Win! User beats Computer";
-            break;
-        default:
-            return "Something went wrong";
-    } 
+    // let randomNumber = Math.floor(Math.random()*2);
+    // switch(randomNumber){
+    //     case 0:
+    //         return "You Lose! Computer Beats User";
+    //         break;
+    //     case 1:
+    //         return "You Win! User beats Computer";
+    //         break;
+    //     default:
+    //         return "Something went wrong";
+    // } 
     // hardcoded this function so that game function can catch the return value from this function
 }
 
 function game(){
     let playResult = "";
-    userCount = 0;
-    computerCount = 0;
     for(let i=0; i<5; i++){
         const playerSelection = window.prompt("To start the play please enter 'rock' or 'paper' or 'scissors'");
-        playResult = this.playRound(playerSelection, computerSelection);
+        const computerSelection = computerPlay();
+        playResult = playRound(playerSelection, computerSelection);
         console.log(playResult)
         if( playResult.includes("You Win")){
             userCount = userCount + 1;
             console.log("Your score: ",userCount);
             console.log("Computer score: ",computerCount);
         }
-        else {
+        else if( playResult.includes("You Lose")) {
             computerCount = computerCount + 1;
             console.log("Computer score: ",computerCount);
             console.log("Your score: ",userCount);
+        }
+        else {
+              i == i-1;
+              console.log("Invalid input");
         }
     }
    
@@ -67,6 +68,9 @@ function game(){
     }
     else if(userCount < computerCount) {
         console.log("Computer Won the Game!");
+    }
+    else if (userCount === computerCount){
+        console.log("Game draw!");
     }
 
 }
