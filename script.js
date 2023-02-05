f1f473dbc226e810aef3b2d2c657f66919f15ac2
Aikorkem <zhumabekaks@gmail.com>
2023-02-05 17:04:51 +0600

    Adjusted variable names

--- script.js
+++ script.js
@@ -36,34 +36,34 @@ function playRound(playerSelection, computerSelection){
 
 function game(){
     let playResult = "";
-    let userCount = 0;
+    let playerCount = 0;
     let computerCount = 0;
     alert("Let's start a game!");
 
     for(let i=0; i<5; i++){
-        alert(`Round ${i+1}\nPlayer = ${userCount} x Computer = ${computerCount}`);
-        let playerSelection = window.prompt(`Please, type your choise:\n - Rock\n - Paper\n - Scissors`);
-        let inputCheck = playerSelection.replace(/\s*/g, "");
+        alert(`Round ${i+1}\nPlayer = ${playerCount} x Computer = ${computerCount}`);
+        let playerInput = window.prompt(`Please, type your choise:\n - Rock\n - Paper\n - Scissors`);
+        let inputCheck = playerInput.replace(/\s*/g, "");
 
-        if(/\b(rock|paper|scissors)\b/i.test(inputCheck) && /(rock|paper|scissors)/i.test(playerSelection)){
-            playerSelection = inputCheck;
+        if(/\b(rock|paper|scissors)\b/i.test(inputCheck) && /(rock|paper|scissors)/i.test(playerInput)){
+            let playerSelection = inputCheck;
             let computerSelection = computerPlay();
             console.log(`Computer chose "${computerSelection}". You chose "${playerSelection}"`);
             playResult = playRound(playerSelection, computerSelection);
             console.log(playResult);
 
             if( playResult.includes("You Win")){
-                userCount++;
-                console.log("Your score: ",userCount);
+                playerCount++;
+                console.log("Your score: ",playerCount);
                 console.log("Computer score: ",computerCount);
             }
             else if( playResult.includes("You Lose")) {
                 computerCount++;
                 console.log("Computer score: ",computerCount);
-                console.log("Your score: ",userCount);
+                console.log("Your score: ",playerCount);
             }else if( playResult.includes("Draw")) {
                 console.log("Computer score: ",computerCount);
-                console.log("Your score: ",userCount);
+                console.log("Your score: ",playerCount);
             }else{
                 console.log("Error");
             }
@@ -73,7 +73,7 @@ function game(){
         }
     }
 
-    finalResult(userCount, computerCount);
+    finalResult(playerCount, computerCount);
 }
 
 function finalResult(playerScore, computerScore) {
