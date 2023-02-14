b61ee5e964cf7e0fac4b645cdafc8dfd054f4a9b
jessicaponciano <j.poncianofonseca@gmail.com>
2023-02-14 16:56:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 const possibleOptions = ["rock", "paper", "scissors"];
-const winner = game();
+let playerCount = 0;
+let computerCount = 0;
+game();
 
 function computerPlay(){
     const computerRandomSelection = possibleOptions[Math.floor(Math.random() * 3)];
     return computerRandomSelection;
+}
+
+function userPlay(i) {
+    alert(`Round ${i+1}\nPlayer = ${playerCount} x Computer = ${computerCount}`);
+    let playerInput = window.prompt("Please, type your choise:\n - Rock\n - Paper\n - Scissors");
+    if (playerInput) {
+        return playerInput;
+    } else {
+        alert("End Game");
+    }
+}
+
+function inputCheck(i, playerInput) {
+    let playResult = "";
+    let inputCheck = playerInput.replace(/\s*/g, "");
+
+    if(/\b(rock|paper|scissors)\b/i.test(inputCheck) && /(rock|paper|scissors)/i.test(playerInput)) {
+        let playerSelection = inputCheck;
+        let computerSelection = computerPlay();
+        console.log(`Computer chose ${computerSelection}. You chose ${playerSelection}`);
+       
+        playResult = playRound(playerSelection, computerSelection);
+        console.log(playResult);
+        return playResult;
+    } else {
+        alert("Invalid input");
+        return i;
+    }
 }
 
 function playRound(playerSelection, computerSelection){
@@ -35,53 +65,39 @@ function playRound(playerSelection, computerSelection){
 }
 
 function game(){
-    let playResult = "";
-    let playerCount = 0;
-    let computerCount = 0;
-    alert("Let's start a game!");
+    alert("Let's start a game! Rock, Paper or Scissors");
+    for(let i=0; i<5; i++) {
+        let playerInput = userPlay(i);
+        let playResult = inputCheck(i, playerInput);
 
-    for(let i=0; i<5; i++){
-        alert(`Round ${i+1}\nPlayer = ${playerCount} x Computer = ${computerCount}`);
-        let playerInput = window.prompt(`Please, type your choise:\n - Rock\n - Paper\n - Scissors`);
-        let inputCheck = playerInput.replace(/\s*/g, "");
-
-        if(/\b(rock|paper|scissors)\b/i.test(inputCheck) && /(rock|paper|scissors)/i.test(playerInput)){
-            let playerSelection = inputCheck;
-            let computerSelection = computerPlay();
-            console.log(`Computer chose "${computerSelection}". You chose "${playerSelection}"`);
-            playResult = playRound(playerSelection, computerSelection);
-            console.log(playResult);
-
-            if( playResult.includes("You Win")){
+        if(typeof playResult === "number") {
+            i--;
+        } else {
+            if(playResult.includes("win")) {
                 playerCount++;
-                console.log("Your score: ",playerCount);
-                console.log("Computer score: ",computerCount);
-            }
-            else if( playResult.includes("You Lose")) {
+                console.log(`Your score: ${playerCount}`);
+                console.log(`Computer score: ${computerCount}`);
+            } else if(playResult.includes("lose")) {
                 computerCount++;
-                console.log("Computer score: ",computerCount);
-                console.log("Your score: ",playerCount);
-            }else if( playResult.includes("Draw")) {
-                console.log("Computer score: ",computerCount);
-                console.log("Your score: ",playerCount);
-            }else{
+                console.log(`Computer score: ${computerCount}`);
+                console.log(`Your score: ${playerCount}`);
+            } else if(playResult.includes("draw")) {
+                    console.log(`Computer score: ${computerCount}`);
+                    console.log(`Your score: ${playerCount}`);
+            } else {
                 console.log("Error");
             }
-        }else{
-            i--;
-            alert("Invalid input");
         }
     }
-
     finalResult(playerCount, computerCount);
 }
 
 function finalResult(playerScore, computerScore) {
     if (playerScore > computerScore) {
-        alert(`Final result: You are the Winner!`);
+        alert("Final result: You are the Winner!");
     } else if (playerScore < computerScore ) {
-        alert(`Final result: You Lost! Try again!`);
+        alert("Final result: You Lost! Try again!");
     } else {
-        alert(`Final result: Draw!`);
+        alert("Final result: Draw!");
     }
 }
